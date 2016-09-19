03a1117da2bb4666bcd03c717499c5d9b44636a8
Abualsamid <ahmad@abualsamid.com>
2016-09-18 23:15:58 -0400

    add to slack

---
+++ bot.js
@@ -0,0 +1,11 @@
+https://github.com/abualsamid/slacklambda
+
+var botBuilder = require('claudia-bot-builder'),
+    excuse = require('huh');
+
+module.exports = botBuilder(function (request) {
+  return 'Hello ' + request.sender + '. Thanks for sending '
+      + request.text  +
+      '. Your message is very important to us, but ' + 
+      excuse.get();
+});
